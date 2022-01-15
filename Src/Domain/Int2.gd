067fef9d4a916bb5e709443cb9153715b4c818f4
CXRedix <1135058679@qq.com>
2022-01-15 23:03:31 +0800

    Fixed Conversion warnings

---
+++ Src/Domain/Int2.gd
@@ -0,0 +1,9 @@
+class_name Int2
+extends Object
+
+func _init(x: int, y: int) -> void:
+	self.x = x
+	self.y = y
+
+var x: int
+var y: int
--- Src/Utilities/TilemapUtils.gd
+++ Src/Utilities/TilemapUtils.gd
@@ -16,7 +16,10 @@ static func set_area_single(startX: int, startY: int, width: int, height: int, i
 		x += 1
 
 static func set_area_singlev(startPosition: Vector2, size: Vector2, id: int, tilemap: TileMap) -> void:
-	set_area_single(startPosition.x, startPosition.y, size.x, size.y, id, tilemap)
+	set_area_single(
+		int(startPosition.x), int(startPosition.y),
+		int(size.x), int(size.y), id, tilemap
+	)
 
 static func set_area_multi(startX: int, startY: int, width: int, height: int, ids: Array, tilemap: TileMap) -> void:
 	var maxX := startX + width
@@ -37,7 +40,10 @@ static func set_area_multi(startX: int, startY: int, width: int, height: int, id
 		x += 1
 
 static func set_area_multiv(startPosition: Vector2, size: Vector2, ids: Array, tilemap: TileMap) -> void:
-	set_area_multi(startPosition.x, startPosition.y, size.x, size.y, ids, tilemap)
+	set_area_multi(
+		int(startPosition.x), int(startPosition.y),
+		int(size.x), int(size.y), ids, tilemap
+	)
 
 static func global_to_map(tilemap: TileMap, position: Vector2) -> Vector2:
 	return tilemap.world_to_map(tilemap.to_local(position))
