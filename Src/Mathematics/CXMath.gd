2eb084b4082319f17418114bd6319fd702ad6106
CXRedix <1135058679@qq.com>
2022-01-18 20:04:42 +0800

    added more math functions

--- Src/Mathematics/CXMath.gd
+++ Src/Mathematics/CXMath.gd
@@ -4,3 +4,12 @@ extends Object
 # maps a value from [inMin ~ inMax] to [outMin ~ outMax]
 static func map(value, inMin, inMax, outMin, outMax):
 	return outMin + ((outMax - outMin) / (inMax - inMin)) * (value - inMin)
+
+static func mini(a: int, b: int) -> int:
+	return a if a < b else b
+
+static func maxi(a: int, b: int) -> int:
+	return a if a > b else b
+
+static func absi(value: int) -> int:
+	return -value if value < 0 else value
