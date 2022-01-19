649a0eb0bd100a3f614261d7dacce34c1acf76bc
CXRedix <1135058679@qq.com>
2022-01-19 16:19:29 +0800

    Fixed tickers

--- Src/Domain/Tickerf.gd
+++ Src/Domain/Tickerf.gd
@@ -2,19 +2,19 @@ class_name Tickerf
 extends Object
 
 func reset() -> void:
-	tick = 0
+	currentTick = 0
 
 func tick(delta: float, maxTick: float) -> bool:
-	tick += delta
+	currentTick += delta
 
-	if delta > maxTick:
-		tick -= maxTick
+	if currentTick > maxTick:
+		currentTick -= maxTick
 		emit_signal("ticked")
 		return true
 	# else
 	
 	return false
 
-var tick: float = 0.0
+var currentTick: float = 0.0
 
 signal ticked
