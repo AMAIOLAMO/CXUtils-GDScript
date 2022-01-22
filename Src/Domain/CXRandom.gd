85c718422e6efa4d1ee8e057acff01ac880e3abc
CXRedix <1135058679@qq.com>
2022-01-22 20:45:29 +0800

    Added more functions to math and random

--- Src/Domain/CXRandom.gd
+++ Src/Domain/CXRandom.gd
@@ -11,6 +11,11 @@ static func rand_point_in_circle(radius: float = 1.0) -> Vector2:
 		resultRadius * sin(resultTheta)
 	)
 
+static func rand_point_in_circle_range(minRange: float, maxRange: float) -> Vector2:
+	var point := rand_point_in_circle()
+	var direction := point.normalized()
+	return point * (maxRange - minRange) + direction * minRange
+
 static func randi_range(minValue: int, maxValue: int) -> int:
 	var diff := maxValue - minValue
 	return minValue if diff == 0 else randi() % diff + minValue
--- Src/Mathematics/CXMath.gd
+++ Src/Mathematics/CXMath.gd
@@ -5,6 +5,12 @@ extends Object
 static func map(value, inMin, inMax, outMin, outMax):
 	return outMin + ((outMax - outMin) / (inMax - inMin)) * (value - inMin)
 
+static func map01(value, outMin, outMax):
+	return value * (outMax - outMin) + outMin
+
+static func map_neg11(value, outMin, outMax):
+	return map01(value * .5 + .5, outMin, outMax)
+
 static func mini(a: int, b: int) -> int:
 	return a if a < b else b
 
